df705dae7ea85076c24a341cb495b5f7176b3a05
AymenFJA <aymenfadhel@yahoo.com>
2020-06-17 10:21:03 -0500

    updating notebooks

--- comet-test/Notebook/rp_analytics.py
+++ comet-test/Notebook/rp_analytics.py
@@ -17,3 +17,17 @@ def get_session_tstamps():
         pilot = session.filter(etype=event_entity, inplace=True)
         time_stamps.append(pilot.ttc)
     return (time_stamps)
+
+def get_unit_tstamps():
+    time_stamps = list()
+    loc = [dI for dI in os.listdir('../RP/sessions/') if os.path.isdir(os.path.join('../RP/sessions/',dI))]
+    for session in range(len(loc)):
+        src = os.path.dirname(loc[session])
+        sid = os.path.basename(loc[session])
+        session = ra.Session(sid=sid, stype='radical.pilot', src=os.path.join('../RP/sessions/',loc[session]))
+        event_entity = 'unit'
+        unit = session.filter(etype=event_entity, inplace=True)
+        print(unit.uid)
+        time_stamps.append(unit.timestamps())
+    return (time_stamps)
+    
