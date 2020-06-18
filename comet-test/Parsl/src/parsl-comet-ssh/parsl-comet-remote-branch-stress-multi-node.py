f762938b82a20b06112bd9b9facaf1c97f3cb03c
Aymen Alsaadi <aymen@js-17-185.jetstream-cloud.org>
2020-06-18 11:25:12 -0500

    adding HTEX multinode

--- comet-test/Parsl/src/parsl-comet-ssh/parsl-comet-remote-branch-stress-multi-node.py
+++ comet-test/Parsl/src/parsl-comet-ssh/parsl-comet-remote-branch-stress-multi-node.py
@@ -7,6 +7,7 @@ parsl.set_stream_logger()
 
 from parsl.config import Config
 from parsl.channels import SSHChannel
+from parsl.launchers import SrunLauncher
 from parsl.providers import SlurmProvider
 from parsl.addresses import address_by_query
 from parsl.executors import HighThroughputExecutor
@@ -16,17 +17,19 @@ from parsl.executors.high_throughput import interchange
 config = Config(
     executors=[
         HighThroughputExecutor(
-            label='comet_htex',
+            label='Comet_HTEX_multinode',
             worker_debug=True,
             address='js-17-185.jetstream-cloud.org',
-            max_workers=24,
+            max_workers=48,
             #workers_per_node = 24, # Getting error for unexpexted argument
             cores_per_worker=1,
             worker_logdir_root = '/home/aymen/parsl_scripts',
             interchange_address='comet-ln2.sdsc.edu',
-            interchange_port_range=(50000, 50400),
-            worker_port_range=(50500, 51000),
+            interchange_port_range=(50000, 51000),
+            #client_ports=(50055, 50056, 50057), # Getting error for unexpexted argument
+            worker_port_range=(50000, 51000),
             provider=SlurmProvider(
+                launcher=SrunLauncher(),
                 #'compute',
                 channel=SSHChannel(
                     hostname='comet-ln2.sdsc.edu',
@@ -34,22 +37,23 @@ config = Config(
                     password='xSeDe2017',
                     script_dir='/home/aymen/parsl_scripts',    # Please replace USERNAME with your username
                 ),
-                #launcher=SrunLauncher(),
+                # launcher=SrunLauncher(),
                 scheduler_options='',     # Input your scheduler_options if needed
                 worker_init='source parsl-env/bin/activate',     # Input your worker_init if needed
-                partition = "debug",
-                walltime="00:10:00",
+                partition = "compute",
+                walltime="00:30:00",
                 init_blocks=1,
                 max_blocks=1,
                 #tasks_per_node = 24, # Getting error for unexpexted argument
-                nodes_per_block=1,
+                nodes_per_block=2,
                 #cores_per_node=24, # Getting error for unexpexted argument
-                parallelism=24,
+                parallelism=48,
             ),
             working_dir="/home/aymen/parsl_scripts",
         )
     ],
-strategy=None,
+strategy='simple',
+usage_tracking=True
 )
 #os.environ['SSH_AUTH_SOCK'] ='/tmp/ssh-sxsiHjHLhr/agent.6773'
 parsl.load(config)
@@ -61,7 +65,7 @@ def stress_ng(outputs=[], stdout='/home/aymen/stress_output/stress_ng.stdout', s
 # loop to execute the simulation app 3 times
 results = []
 
-for i in range(24):
+for i in range(48):
     out_file = "/home/aymen/stress_output/stress_ng_{0}".format(i)
     results.append(stress_ng(outputs=[out_file]))
 
