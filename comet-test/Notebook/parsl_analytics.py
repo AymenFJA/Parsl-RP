7059f746ec04b43ad3d7620cc46ad3871d2e593e
Aymen Alsaadi <aymen@js-17-185.jetstream-cloud.org>
2020-06-29 10:57:53 -0500

    adding plots for 1728 task for both parsl and RP

--- comet-test/Notebook/parsl_analytics.py
+++ comet-test/Notebook/parsl_analytics.py
@@ -1,6 +1,7 @@
 import re
 import os
 import subprocess
+import pandas as pd
 from datetime import datetime
 
 def stamp_to_num(ss):
@@ -10,26 +11,30 @@ def stamp_to_num(ss):
     return time
 
 def get_session_tstamps():
-        
+       
+        s    = list()
         dur  = list()
-        line = "/home/aymen/SummerRadical/Parsl-RP/comet-test/Parsl/parsl_sessions"
+        line = "/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions"
         cmd  =  "find %s -iname 'interchange.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
+            session_id = (paths[f].decode().split('/')[8])
             with open(paths[f], 'rb') as fh:
                 first = next(fh).decode()
                 fh.seek(-4096, 2)
                 last = fh.readlines()[-1].decode()
                 f = first.split()[1]
                 l = last.split()[1]
+                s.append(session_id)
                 dur.append(stamp_to_num(l) - stamp_to_num(f))
-        print (dur)
-        return dur
+        df = pd.DataFrame(list(zip(s, dur)), columns =['Session', 'Duration'])
+        dd = df.sort_values('Duration')
+        return(dd.reset_index(drop=True))
 
 def get_workers_tstamps():
         
         dur = list()
-        line = '/home/aymen/SummerRadical/Parsl-RP/comet-test/Parsl/parsl_sessions'
+        line = '/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions'
         cmd  =  "find %s -iname 'worker_*.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
@@ -46,7 +51,7 @@ def get_workers_tstamps():
 def get_manager_tstamps():
 
         dur  = list()
-        line ='/home/aymen/SummerRadical/Parsl-RP/comet-test/Parsl/parsl_sessions'
+        line ='/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions'
         cmd  =  "find %s -iname 'manager.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
--- comet-test/Notebook/rp_analytics.py
+++ comet-test/Notebook/rp_analytics.py
@@ -1,22 +1,29 @@
 import os
 import sys
 import pprint
+import pandas as pd
 import radical.pilot as rp
 import radical.utils     as ru
 import radical.analytics as ra
 
 def get_session_tstamps():
     
+    s    = list()
     time_stamps = list()
     loc = [dI for dI in os.listdir('../RP/sessions/') if os.path.isdir(os.path.join('../RP/sessions/',dI))]
     for session in range(len(loc)):
+        session_id = (loc[session])
         src = os.path.dirname(loc[session])
         sid = os.path.basename(loc[session])
         session = ra.Session(sid=sid, stype='radical.pilot', src=os.path.join('../RP/sessions/',loc[session]))
         event_entity = 'pilot'
         pilot = session.filter(etype=event_entity, inplace=True)
+        s.append(session_id)
         time_stamps.append(pilot.ttc)
-    return (time_stamps)
+        
+    df = pd.DataFrame(list(zip(s, time_stamps)), columns =['Session', 'Duration'])
+    dd = df.sort_values('Duration')
+    return (dd.reset_index(drop=True))
 
 def get_unit_tstamps():
     time_stamps = list()
