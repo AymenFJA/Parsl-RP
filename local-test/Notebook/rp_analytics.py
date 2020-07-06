e58404a109bc30b1257a6808cbb4a7eeaf8cb324
Aymen Alsaadi <aymen@js-17-185.jetstream-cloud.org>
2020-07-06 10:29:24 -0500

    fixed ughhh

---
+++ local-test/Notebook/rp_analytics.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pprint
+import radical.pilot as rp
+import radical.utils     as ru
+import radical.analytics as ra
+
+def get_session_tstamps():
+    
+    time_stamps = list()
+    loc = [dI for dI in os.listdir('../RP/sessions/') if os.path.isdir(os.path.join('../RP/sessions/',dI))]
+    for session in range(len(loc)):
+        src = os.path.dirname(loc[session])
+        sid = os.path.basename(loc[session])
+        session = ra.Session(sid=sid, stype='radical.pilot', src=os.path.join('../RP/sessions/',loc[session]))
+        event_entity = 'pilot'
+        pilot = session.filter(etype=event_entity, inplace=True)
+        time_stamps.append(pilot.ttc)
+    return (time_stamps)
