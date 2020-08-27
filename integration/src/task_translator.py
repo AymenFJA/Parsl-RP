96d0781c7f0ff7d24f8a0a61d2def2fc240197a4
AymenFJA <aymen.alsaadi@rutgers.edu>
2020-08-27 18:43:07 -0500

    fixing plots and adding preliminary code model of RP-ParSL

---
+++ integration/src/task_translator.py
@@ -0,0 +1,33 @@
+import radical.pilot as rp
+import radical.utils as ru
+
+tasks = {}
+parsl_task_description = [executable, self.tasks[task_id]['resource_specification'], *args, **kwargs]
+
+class TaskTranslator(object):
+
+        #-------------------------------------------------------------------------------------------------------------------------#
+        # ParSL API                       |                   | ParSL DFK/dflow |                     |      Task Translator      |
+        #---------------------------------|                   |-----------------|                     |---------------------------|
+        #                                 |                   |                 |                     |                           |
+        # parsl_tasks_description ------>  ParSL_tasks{}--->    Dep. check       --> ParSL_tasks{} -->      ParSL Task/Tasks desc.|
+        #                                 |                   | Data management |                     |             |             |
+        #                                 |                   |                 |                     |             v             |
+        #                                 |                   |                 |                     |     RP Unit/Units desc.   |
+        #-------------------------------------------------------------------------------------------------------------------------#
+
+    def __init__(self):
+
+        pass
+
+    def task_translator(self, parsl_task_description):
+
+        cud = rp.ComputeUnitDescription()
+        cuds = list()
+        cud.executable = parsl_task_description[0]
+        cud.cores      = parsl_task_description[1]['cores'] #assuming that the resource specification first item is cores .... TBD
+        cuds.append(cud)
+
+        return cuds
+
+
