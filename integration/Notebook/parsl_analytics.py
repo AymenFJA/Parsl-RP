b2be162291d75106f78475aab797a992e039d920
AymenFJA <aymen.alsaadi@rutgers.edu>
2020-11-02 10:18:32 -0600

    new data for RP and the integration

--- integration/Notebook/parsl_analytics.py
+++ integration/Notebook/parsl_analytics.py
@@ -14,19 +14,25 @@ def get_session_tstamps():
        
         s    = list()
         dur  = list()
-        line = "/home/aymen/RADICAL/radical.pilot/examples/parsl/WS/runinfo/000/parsl.log"
+        line = "/home/aymen/RADICAL/radical.pilot/examples/parsl/WS"
         cmd  =  "find %s -iname 'parsl.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
             session_id = (paths[f].decode().split('/')[9])
             with open(paths[f], 'rb') as fh:
-                first = next(fh).decode()
-                fh.seek(0, os.SEEK_END)
-                last = fh.readlines()[-1].decode()
-                f = first.split()[1]
-                l = last.split()[1]
-                s.append(session_id)
-                dur.append(stamp_to_num(l) - stamp_to_num(f))
+                print(paths[f])
+
+                first_line = fh.readline()
+                print(first_line)
+                for last_line in fh:
+                    if 'parsl.dataflow.dflow:846' in last_line.split():
+                        print(last_line.split())
+                        f = first_line.split()[1]
+                        l = last_line.split()[1]
+                        s.append(session_id)
+                        dur.append(stamp_to_num(l) - stamp_to_num(f))
+                    else:
+                         print ("something is wrong ")
         df = pd.DataFrame(list(zip(s, dur)), columns =['Session', 'Duration'])
         dd = df.sort_values('Duration')
         return(dd.reset_index(drop=True))
