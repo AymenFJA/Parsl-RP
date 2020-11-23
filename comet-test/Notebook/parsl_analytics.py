465bc0281e7c063c08a3a92f98ced8494e0b9573
AymenFJA <aymen.alsaadi@rutgers.edu>
2020-11-23 10:04:22 -0600

    adding new files

--- comet-test/Notebook/parsl_analytics.py
+++ comet-test/Notebook/parsl_analytics.py
@@ -10,15 +10,40 @@ def stamp_to_num(ss):
     time = datetime.strptime(ss, DB_DATE_FORMAT).timestamp()
     return time
 
+def get_queue_tstamps(s_path):
+    s    = list()
+    t    = list()
+    dur  = 0
+    file = s_path
+    with open(file, 'rb') as fh:
+        for line in fh:
+            if re.match("(.*)0/0/1(.*)",str(line)):
+                time_stmp = line.decode().split()[1]
+                try:
+                    t.append(stamp_to_num(time_stmp))
+                except Exception as e:
+                    print(e)
+            else:
+                pass
+        try:
+            dur = t[-1]-t[0]
+        except IndexError:
+            dur = 0
+    return dur
+
+                
+                
+
+
 def get_session_tstamps():
        
         s    = list()
         dur  = list()
-        line = "/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions"
+        line = "/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions/SS"
         cmd  =  "find %s -iname 'parsl.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
-            session_id = (paths[f].decode().split('/')[8])
+            session_id = (paths[f].decode().split('/')[9])
             with open(paths[f], 'rb') as fh:
                 first = next(fh).decode()
                 fh.seek(-4096, 2)
@@ -35,11 +60,11 @@ def get_interchange_tstamps():
        
         s    = list()
         dur  = list()
-        line = "/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions"
+        line = "/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions/SS"
         cmd  =  "find %s -iname 'interchange.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
-            session_id = (paths[f].decode().split('/')[8])
+            session_id = (paths[f].decode().split('/')[9])
             with open(paths[f], 'rb') as fh:
                 first = next(fh).decode()
                 fh.seek(-4096, 2)
@@ -56,11 +81,11 @@ def get_workers_tstamps():
         
         s   = list()
         dur = list()
-        line = '/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions'
+        line = '/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions/SS'
         cmd  =  "find %s -iname 'worker_*.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
-            session_id = (paths[f].decode().split('/')[8])
+            session_id = (paths[f].decode().split('/')[9])
             with open(paths[f], 'rb') as fh:
                 first = next(fh).decode()
                 last = fh.readlines()[-1].decode()
@@ -77,11 +102,11 @@ def get_manager_tstamps():
     
         s    = list()
         dur  = list()
-        line ='/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions'
+        line ='/home/aymen/RADICAL/Parsl-RP/comet-test/Parsl/parsl_sessions/SS'
         cmd  =  "find %s -iname 'manager.log'" %line
         paths = [line for line in subprocess.check_output(cmd , shell=True).splitlines()]
         for f in range(len(paths)):
-            session_id = (paths[f].decode().split('/')[8])
+            session_id = (paths[f].decode().split('/')[9])
             with open(paths[f], 'rb') as fh:
                 first = next(fh).decode()
                 last = fh.readlines()[-1].decode()
